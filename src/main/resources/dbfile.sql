1cb78cc9112eb5e177b48b95c799f5bd3885a488
Kalyan <kalyankumarreddy098@gmail.com>
2021-11-02 20:02:24 +0530

    dbfile added

---
+++ src/main/resources/dbfile.sql
@@ -0,0 +1,6 @@
+INSERT INTO `project`.`product` (`productid`, `catagory`, `name`, `price`) VALUES ('1', 'Laptops', 'Asus', '52031');
+INSERT INTO `project`.`product` (`productid`, `catagory`, `name`, `price`) VALUES ('2', 'Laptops', 'Asus Gen', '50000');
+INSERT INTO `project`.`product` (`productid`, `catagory`, `name`, `price`) VALUES ('3', 'Laptops', 'Hp', '25099');
+INSERT INTO `project`.`product` (`productid`, `catagory`, `name`, `price`) VALUES ('4', 'Mobiles', 'Cool Pad Note 3', '25999');
+INSERT INTO `project`.`product` (`productid`, `catagory`, `name`, `price`) VALUES ('5', 'Mobiles', 'Redmi', '10499');
+INSERT INTO `project`.`product` (`productid`, `catagory`, `name`, `price`) VALUES ('6', 'Mobiles', 'Samsung M30', '12999');
